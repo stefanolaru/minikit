a79d40c66d084408cfe6481119702055a7094809
Stefan Olaru <contact@stefanolaru.com>
2013-11-09 11:54:21 +0200

    new shortcode [contact-form]

--- minikit/shortcodes.php
+++ minikit/shortcodes.php
@@ -44,4 +44,54 @@ function email_antispam($atts,$content=null) {
 }
 add_shortcode('email', 'email_antispam');
 
+/* contact form */
+function mk_contact_form($atts,$content=null) {
+	$html = '<div class="mkcf-wrapper">';
+	
+	// check for validation errors
+	if(!empty($_SESSION['mk-validation-errors'])) {
+		$html .= '<div class="alert-box error">';
+		$html .= implode('<br />', $_SESSION['mk-validation-errors']);
+		$html .= '</div>';
+		// unset validation errors
+		unset($_SESSION['mk-validation-errors']);
+	}
+	
+	$html .= '<form method="post" action="'.currentURL().'" id="mk-contact-form">';
+	// name field
+	$html .= '<div class="input">';
+	$html .= '<label for="mk-name">Name</label>';
+	$html .= '<input type="text" class="input-text" id="mk-name" name="mk-name" value="" />';
+	$html .= '</div>';
+	
+	// email field
+	$html .= '<div class="input">';
+	$html .= '<label for="mk-email">Email</label>';
+	$html .= '<input type="email" class="input-text" id="mk-email" name="mk-email" value="" />';
+	$html .= '</div>';
+	
+	// spam	bait field
+	$html .= '<div class="input" style="display:none;">';
+	$html .= '<label for="mk-url">URL</label>';
+	$html .= '<input type="text" class="input-text" id="mk-url" name="url" value="" />';
+	$html .= '</div>';
+	
+	// message field
+	$html .= '<div class="input">';
+	$html .= '<label for="mk-message">Message</label>';
+	$html .= '<textarea name="mk-message" rows="5" id="mk-message" class="input-text"></textarea>';
+	$html .= '</div>';
+	
+	// submit
+	$html .= '<div class="input">';
+	$html .= '<input type="hidden" value="'.encrypt(json_encode($atts), ENCRYPTION_KEY).'" name="mk-atts" />';
+	$html .= '<input type="submit" value="Send" name="mk-contact" class="button" />';
+	$html .= '</div>';
+	$html .= '</form>';
+	$html .= '</div>';
+	return $html;
+}
+add_shortcode('contact-form', 'mk_contact_form');
+
+
 ?>
