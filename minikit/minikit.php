b32165cf7a64c708d43e0f9ec06e8185f19a30f5
Stefan Olaru <contact@stefanolaru.com>
2014-04-08 17:37:25 +0300

    currentURL() HTTPS fix

--- minikit/minikit.php
+++ minikit/minikit.php
@@ -147,7 +147,7 @@ function changeWPURL($old_url,$new_url) {
 // get current URL
 function currentURL() {
 	 $pageURL = 'http';
-	 if ($_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
+	 if (isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
 	 $pageURL .= "://";
 	 if ($_SERVER["SERVER_PORT"] != "80") {
 	  $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
