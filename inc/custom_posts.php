2c7fbec0e1a25bf46aeaeaef45deca49eed529af
Stefan Olaru <contact@stefanolaru.com>
2015-09-08 10:20:30 +0300

    updated LESS mixins, added change posts to news function

--- inc/custom_posts.php
+++ inc/custom_posts.php
@@ -4,6 +4,10 @@ class MinikitCustomPosts extends Minikit {
 	
 	function __construct() {
 		add_action('init', array($this, 'create_custom_posts'));
+		
+		// rename admin Posts into News (or whatever)
+//		add_action( 'init', array($this, 'change_post_object_label') );
+//		add_action( 'admin_menu', array($this, 'change_post_menu_label') );
 	}
 	
 	function create_custom_posts() {
@@ -33,6 +37,30 @@ class MinikitCustomPosts extends Minikit {
 			'parent_item_colon' => ''
 		);
 		
+	}
+	
+	function change_post_menu_label() {
+		global $menu;
+		global $submenu;
+		$menu[5][0] = 'News';
+		$submenu['edit.php'][5][0] = 'News';
+		$submenu['edit.php'][10][0] = 'Add News';
+		$submenu['edit.php'][16][0] = 'News Tags';
+		echo '';
+	}
+	function change_post_object_label() {
+		global $wp_post_types;
+		$labels = &$wp_post_types['post']->labels;
+		$labels->name = 'News';
+		$labels->singular_name = 'News';
+		$labels->add_new = 'Add News';
+		$labels->add_new_item = 'Add News';
+		$labels->edit_item = 'Edit News';
+		$labels->new_item = 'News';
+		$labels->view_item = 'View News';
+		$labels->search_items = 'Search News';
+		$labels->not_found = 'No News found';
+		$labels->not_found_in_trash = 'No News found in Trash';
 	}
 	
 }
